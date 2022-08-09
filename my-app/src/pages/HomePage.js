9414d33c9ceb1e23d6c5b998452929f76efb28f7
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-08-09 19:54:36 +0700

    #4 footer done

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -5,20 +5,20 @@ import { Card } from "../components/card";
 const HomePage = () => {
   return (
     <div className="container">
-      <div className="flex justify-center items-center mt-10">
+      <div className="flex items-center justify-between mt-10">
         <div>
           <div className="text-5xl font-bold">
             Discover, Sell
             <br></br>& Collect Rare NFTs
           </div>
-          <div className="text-base mt-4 mb-10">
+          <div className="mt-4 mb-10 text-base">
             Digital marketplace for crypto collections and non-fungible tokens
             (NFTs)
           </div>
           <Button kind="primary" height={"56px"} width={"300px"}>
             Discover now
           </Button>
-          <div className="mt-10 flex justify-between items-center">
+          <div className="flex items-center justify-between mt-10">
             <div>
               <div className="text-5xl font-bold">460 301</div>
               <div>NFTs</div>
@@ -31,14 +31,14 @@ const HomePage = () => {
         </div>
         <img src={require("../img/Devera-brand-character-1.png")} alt="" />
       </div>
-      <div className="flex justify-end items-center gap-x-10 my-10">
+      <div className="flex items-center justify-end my-10 gap-x-10">
         <img src={require("../img/logo-dark-1.png")} alt="" />
         <img src={require("../img/Devera-logo-red-1.png")} alt="" />
         <img src={require("../img/Lecle-logo-yellow-1.png")} alt="" />
       </div>
-      <div className="flex justify-center flex-col p-10 border-t border-t-zinc-400 border-opacity-20">
-        <div className="text-xl text-left my-10 ml-10">Super Hot</div>
-        <div className="gap-x-8 flex justify-center items-center text-sm">
+      <div className="flex flex-col justify-center p-10 border-t border-t-zinc-400 border-opacity-20">
+        <div className="my-10 ml-10 text-xl text-left">Super Hot</div>
+        <div className="flex items-center justify-center text-sm gap-x-8">
           <Button kind="secondary" height={"34px"}>
             Music
           </Button>
@@ -58,7 +58,7 @@ const HomePage = () => {
             Music
           </Button>
         </div>
-        <div className="grid grid-cols-3 gap-x-10 gap-y-12 mx-auto">
+        <div className="grid grid-cols-3 mx-auto gap-x-10 gap-y-12">
           <Card to={"/"}></Card>
           <Card to={"/"}></Card>
           <Card to={"/"}></Card>
