8f18020d9c684e5f61d9f81de2a501866ac164cb
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-08-14 21:15:20 +0700

    #12 BuyPage

--- my-app/src/pages/BuyPage.js
+++ my-app/src/pages/BuyPage.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { Card } from "../components/card";
+import { Button } from "../components/button";
+import { NavLink } from "react-router-dom";
 const BuyPage = () => {
-  return <div>Buy Page</div>;
+  console.log('buy page')
+  return <div className="container " >
+      <div className="flex flex-col items-center justify-center mt-3">
+        <div className="mb-4 heading-text">Buy now!</div>
+        <Card to={"/"}></Card>
+        <div className="my-6 message-text">Hurry up, you will be late!</div>
+        <Button kind="primary" width={"183px"}>Buy</Button>
+      </div>
+      <div className="my-4 flex flex-col justify-center">
+        <div className="mb-6 text-left heading-text">Move from this user</div>
+        <div className="grid grid-cols-4 mx-auto gap-x-10">
+          <Card to={"/"}></Card>
+          <Card to={"/"}></Card>
+          <Card to={"/"}></Card>
+          <Card to={"/"}></Card>
+        </div>
+        <NavLink className={"text-right my-3"} to={"/"}>View all... </NavLink>
+      </div>
+  </div>;
 };
 
 export default BuyPage;
