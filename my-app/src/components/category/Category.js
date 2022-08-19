23e557e77808f9cce5254e355285c16fd5d31974
Nguyễn Ngọc Quỳnh Giao <giaonguyennq142001@gmail.com>
2022-08-19 12:43:05 +0700

    #35 & #47 filter products by category

--- my-app/src/components/category/Category.js
+++ my-app/src/components/category/Category.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'components/button';
+import { Pagination } from 'components/pagination';
 
+const categories = [
+  "Music",
+  "Art",
+  "Sport",
+  "Photography",
+  "Virtual Reality",
+  "Avatar",
+];
+ 
 const Category = () => {
-    return (
-      <div className="categories flex items-center justify-center text-sm gap-x-10">
-        <Button className="category-item" kind="secondary" height={"34px"} active={true}>
-          Music
-        </Button>
-        <Button kind="secondary" height={"34px"}>
-          Art
-        </Button>
-        <Button kind="secondary" height={"34px"}>
-          Sport
-        </Button>
-        <Button kind="secondary" height={"34px"}>
-          Photography
-        </Button>
-        <Button kind="secondary" height={"34px"}>
-          Virtual Reality
-        </Button>
-        <Button kind="secondary" height={"34px"}>
-          Video
-        </Button>
-      </div>
+  const [selectedCategory, setSelectedCategogy] = useState(categories[0]);
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:1337/products").then((response) =>
+      response.json().then((data) => {
+        setProductList(data);
+      })
     );
+  }, []);
+ 
+  return (
+    <div>
+      <div className="categories flex items-center justify-center text-sm gap-x-10">
+        {categories.map((category) => (
+          <Button
+            kind="secondary"
+            height={"34px"}
+            key={category}
+            active={selectedCategory === category}
+            onClick={() => setSelectedCategogy(category)}
+          >
+            {category}
+          </Button>
+        ))}
+      </div>
+      <div className="my-5">
+        <Pagination items={productList.filter(products => products.Category === selectedCategory)}></Pagination>
+      </div>
+    </div>
+  );
 }
 
 export default Category;
--- my-app/src/pages/Marketplace/Marketplace.js
+++ my-app/src/pages/Marketplace/Marketplace.js
@@ -1,28 +1,13 @@
-import { Button } from "components/button";
-import { Card } from "components/card";
 import { Category } from "components/category";
 import { Pagination } from "components/pagination";
 import React, { useEffect, useState } from "react";
 
 const Marketplace = () => {
-  const [ products, setProducts ] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:1337/products").then(
-      response => response.json().then(data => {
-        setProducts(data);
-      })
-    )
-  }, []);
-  console.log(products);
 
   return (
     <div className="container">
       <div className="flex flex-col justify-center border-t border-t-zinc-400 border-opacity-20 my-10 pt-10">
         <Category />
-        <div className="my-5">
-          <Pagination items={products}></Pagination>
-        </div>
       </div>
     </div>
   );
