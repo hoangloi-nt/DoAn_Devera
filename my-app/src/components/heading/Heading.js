95f55bc9582da807b40e75e1dcf157c42f8fac05
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-08-21 21:00:55 +0700

    #37 Add pagination in collection

--- my-app/src/components/heading/Heading.js
+++ my-app/src/components/heading/Heading.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Heading = ({ title = "", desc = "", children }) => {
-  return (
-    <div className="mt-10 flex items-start justify-between border-b border-b-zinc-400 border-opacity-20 ">
-      <div>
-        <h1 className="text-2xl font-semibold">{title}</h1>
-        <p>{desc}</p>
-      </div>
-      {children}
-    </div>
-  );
+	return (
+		<div className="flex items-start justify-between mt-16 border-b border-b-zinc-400 border-opacity-20 ">
+			<div>
+				<h1 className="text-2xl font-semibold">{title}</h1>
+				<p>{desc}</p>
+			</div>
+			{children}
+		</div>
+	);
 };
 
 export default Heading;
--- my-app/src/pages/Collection/Collection.js
+++ my-app/src/pages/Collection/Collection.js
@@ -4,101 +4,75 @@ import { useAuth } from "components/contexts/auth-context";
 import { Card } from "components/card";
 import { Button } from "components/button";
 import { Heading } from "components/heading";
+import { Pagination } from "components/pagination";
 const Collection = () => {
-  const [productList, setProductList] = useState([]);
-  const { userInfo } = useAuth();
-  const [option, setOption] = useState("create");
-  let createProduct = [];
-  let buyProduct = [];
-  useEffect(() => {
-    fetch("http://localhost:1337/products").then((response) =>
-      response.json().then((data) => {
-        setProductList(data);
-      })
-    );
-  }, []);
-  productList.forEach((item) => {
-    if (item.createby.id === userInfo.id) {
-      createProduct.push(item);
-    }
-    if (item.boughtby) {
-      if (item.boughtby.id === userInfo.id) {
-        buyProduct.push(item);
-      }
-    }
-  });
-  useEffect(() => {
-    document.title = "Collection";
-  }, []);
-  return (
-    <div className="container ">
-      <Heading
-        title="Your Collection"
-        desc="You can find the products you created and bought."
-      ></Heading>
-      <div className="flex flex-col justify-center my-4 mt-10 ">
-        <div className="flex option">
-          <Button
-            className="mb-6t"
-            onClick={() => setOption("create")}
-            active={option === "create"}
-            kind={"secondary"}
-            width="180px"
-          >
-            Created Product
-          </Button>
-          <Button
-            className="mb-6 ml-10"
-            onClick={() => setOption("buy")}
-            active={option === "buy"}
-            kind={"secondary"}
-            width="180px"
-          >
-            Buy Product
-          </Button>
-        </div>
-        {option === "create" ? (
-          createProduct.length !== 0 ? (
-            <div className="grid grid-cols-4 gap-10 mx-auto">
-              {createProduct.map((item) => (
-                <Card
-                  key={item.id}
-                  to={"/"}
-                  image={item.image}
-                  title={item.Name}
-                  address={item.createby.address}
-                  price={item.Price}
-                  avatar={item.createby.avatar}
-                ></Card>
-              ))}
-            </div>
-          ) : (
-            <div className="my-16 heading-text">
-              You have not created any products yet
-            </div>
-          )
-        ) : buyProduct.length !== 0 ? (
-          <div className="grid grid-cols-4 gap-10 mx-auto">
-            {buyProduct.map((item) => (
-              <Card
-                key={item.id}
-                to={"/"}
-                image={item.image}
-                title={item.Name}
-                address={item.createby.address}
-                price={item.Price}
-                avatar={item.createby.avatar}
-              ></Card>
-            ))}
-          </div>
-        ) : (
-          <div className="my-16 heading-text">
-            You have not purchased any products yet
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	const [productList, setProductList] = useState([]);
+	const { userInfo } = useAuth();
+	const [option, setOption] = useState("create");
+	let createProduct = [];
+	let buyProduct = [];
+	useEffect(() => {
+		fetch("http://localhost:1337/products").then((response) =>
+			response.json().then((data) => {
+				setProductList(data);
+			}),
+		);
+	}, []);
+	productList.forEach((item) => {
+		if (item.createby.id === userInfo.id) {
+			createProduct.push(item);
+		}
+		if (item.boughtby) {
+			if (item.boughtby.id === userInfo.id) {
+				buyProduct.push(item);
+			}
+		}
+	});
+	useEffect(() => {
+		document.title = "Collection";
+	}, []);
+	return (
+		<div className="container ">
+			<Heading title="Your Collection"></Heading>
+			<div className="flex flex-col justify-center my-4 mt-10 ">
+				<div className="flex option">
+					<Button
+						className="mb-6t"
+						onClick={() => setOption("create")}
+						active={option === "create"}
+						kind={"secondary"}
+						width="180px"
+					>
+						Created Product
+					</Button>
+					<Button
+						className="mb-6 ml-10"
+						onClick={() => setOption("buy")}
+						active={option === "buy"}
+						kind={"secondary"}
+						width="180px"
+					>
+						Buy Product
+					</Button>
+				</div>
+				{option === "create" ? (
+					createProduct.length !== 0 ? (
+						<Pagination items={createProduct}></Pagination>
+					) : (
+						<div className="my-12 heading-text">
+							You have not created any products yet
+						</div>
+					)
+				) : buyProduct.length !== 0 ? (
+					<Pagination items={buyProduct}></Pagination>
+				) : (
+					<div className="my-12 heading-text">
+						You have not purchased any products yet
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Collection;
