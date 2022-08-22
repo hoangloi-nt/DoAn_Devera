19ad0818f0d4f0895d18a3eb9e631c2e392be1d2
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-08-22 21:56:48 +0700

    Update heading BuyPage Collection

--- my-app/src/pages/BuyPage.js
+++ my-app/src/pages/BuyPage.js
@@ -9,153 +9,158 @@ import { useState } from "react";
 import { transfer } from "sdk/iconSDK.js";
 import { useAuth } from "components/contexts/auth-context";
 const BuyPage = () => {
-  const { nftId } = useParams();
-  const [productData, setProductData] = useState({});
-  const [listProductData, setListProductData] = useState([]);
-  const [sold, setSold] = useState(false);
-  const { userInfo } = useAuth();
-  const [checkUser, setCheckUser] = useState(false);
-  const sendToken = async (address, price) => {
-    const transferSuccess = await transfer({
-      to: address,
-      value: price,
-    });
+	const { nftId } = useParams();
+	const [productData, setProductData] = useState({});
+	const [listProductData, setListProductData] = useState([]);
+	const [sold, setSold] = useState(false);
+	const { userInfo } = useAuth();
+	const [checkUser, setCheckUser] = useState(false);
+	const sendToken = async (address, price) => {
+		const transferSuccess = await transfer({
+			to: address,
+			value: price,
+		});
 
-    if (transferSuccess === true) {
-      console.log("Done");
-      updateProduct(nftId, {
-        //user
-        id: userInfo.id,
-      });
-      setSold(true);
-    }
-  };
+		if (transferSuccess === true) {
+			console.log("Done");
+			updateProduct(nftId, {
+				//user
+				id: userInfo.id,
+			});
+			setSold(true);
+		}
+	};
 
-  const onSubmit = async (address, price) => {
-    sendToken(address, price);
-  };
-  useEffect(() => {
-    async function fetchProductData() {
-      const product = await axios.get(
-        `http://localhost:1337/products/${nftId}`
-      );
-      setProductData(product.data);
-      if (product.data.boughtby) {
-        setSold(true);
-      }
-    }
-    fetchProductData();
-    document.body.scrollIntoView({ behavior: "smooth", block: "start" });
-  }, [nftId]);
+	const onSubmit = async (address, price) => {
+		sendToken(address, price);
+	};
+	useEffect(() => {
+		async function fetchProductData() {
+			const product = await axios.get(
+				`http://localhost:1337/products/${nftId}`,
+			);
+			setProductData(product.data);
+			if (product.data.boughtby) {
+				setSold(true);
+			}
+		}
+		setSold(false);
+		fetchProductData();
+		document.body.scrollIntoView({ behavior: "smooth", block: "start" });
+	}, [nftId]);
 
-  async function updateProduct(id, data) {
-    try {
-      await axios.put(`http://localhost:1337/products/${id}`, {
-        boughtby: data,
-      });
-      toast.success("Buy success");
-    } catch (error) {
-      console.log(error);
-      toast.error("Buy failed!", error.message);
-    }
-  }
-  const creatorId = productData?.createby?.id;
+	async function updateProduct(id, data) {
+		try {
+			await axios.put(`http://localhost:1337/products/${id}`, {
+				boughtby: data,
+			});
+			toast.success("Buy success");
+		} catch (error) {
+			console.log(error);
+			toast.error("Buy failed!", error.message);
+		}
+	}
+	const creatorId = productData?.createby?.id;
 
-  useEffect(() => {
-    if (!creatorId) return;
-    async function fetchCreatorData() {
-      const creator = await axios.get(
-        `http://localhost:1337/creators/${creatorId}`
-      );
-      let result = [];
-      creator.data.create.forEach((item) => {
-        if (item.id !== parseInt(nftId)) {
-          result.push(item);
-        }
-      });
-      setListProductData(result);
-    }
-    fetchCreatorData();
-    if (creatorId === userInfo.id) setCheckUser(true);
-  }, [creatorId, nftId, userInfo.id]);
+	useEffect(() => {
+		if (!creatorId) return;
+		async function fetchCreatorData() {
+			const creator = await axios.get(
+				`http://localhost:1337/creators/${creatorId}`,
+			);
+			let result = [];
+			creator.data.create.forEach((item) => {
+				if (item.id !== parseInt(nftId)) {
+					result.push(item);
+				}
+			});
+			setListProductData(result);
+		}
+		fetchCreatorData();
+		if (creatorId === userInfo.id) setCheckUser(true);
+	}, [creatorId, nftId, userInfo.id]);
 
-  useEffect(() => {
-    document.title = "Buy Page";
-  }, []);
+	useEffect(() => {
+		document.title = "Buy Page";
+	}, []);
 
-  return (
-    <div className="container ">
-      <div className="flex flex-col items-center justify-center mt-3">
-        <div className="mb-4 heading-text">Buy now!</div>
-        <Card
-          to={"#"}
-          image={productData?.image}
-          title={productData?.Name}
-          address={productData?.createby?.address}
-          price={productData?.Price}
-          avatar={productData?.createby?.avatar}
-        ></Card>
-        <div className="my-6 message-text">Hurry up, you will be late!</div>
-        {sold
-          ? !checkUser && (
-              <Button kind="primary" width={"183px"} disabled>
-                Sold out
-              </Button>
-            )
-          : !checkUser && (
-              <Button
-                kind="primary"
-                width={"183px"}
-                onClick={() =>
-                  onSubmit(
-                    productData.createby.address,
-                    Number(productData.Price)
-                  )
-                }
-              >
-                Buy
-              </Button>
-            )}
-        {checkUser && (
-          <div className="flex justify-center items-center gap-3 flex-col">
-            <span className="font-bold px-3 py-2 rounded-lg bg-red-400">
-              This is your product, you can't buy it!
-            </span>
-            <Button
-              type="button"
-              kind="primary"
-              to={`/update/${productData.id}`}
-            >
-              Update product
-            </Button>
-          </div>
-        )}
-      </div>
-      <div className="flex flex-col justify-center my-4">
-        <div className="mb-6 text-left heading-text">More from this user</div>
-        <div className="grid grid-cols-4 gap-10 mx-auto">
-          {listProductData.slice(0, 8).map((item) => (
-            <Card
-              key={item.id}
-              to={`/buy/${item.id}`}
-              image={item?.image}
-              title={item?.Name}
-              address={productData?.createby?.address}
-              price={item?.Price}
-              avatar={productData?.createby?.avatar}
-            ></Card>
-          ))}
-        </div>
-        {listProductData.length > 8 ? (
-          <NavLink className={"text-right my-3"} to={"/"}>
-            View all...{" "}
-          </NavLink>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="container ">
+			<div className="flex flex-col items-center justify-center mt-3">
+				{sold ? (
+					<div className="mb-4 heading-text">Sold out!</div>
+				) : checkUser ? (
+					<div className="mb-4 heading-text">This is your product!</div>
+				) : (
+					<div className="mb-4 heading-text">Buy now!</div>
+				)}
+
+				<Card
+					to={"#"}
+					image={productData?.image}
+					title={productData?.Name}
+					address={productData?.createby?.address}
+					price={productData?.Price}
+					avatar={productData?.createby?.avatar}
+				></Card>
+				{sold
+					? !checkUser && <></>
+					: !checkUser && (
+							<>
+								<div className="my-6 message-text">
+									Hurry up, you will be late!
+								</div>
+								<Button
+									kind="primary"
+									width={"183px"}
+									onClick={() =>
+										onSubmit(
+											productData.createby.address,
+											Number(productData.Price),
+										)
+									}
+								>
+									Buy
+								</Button>
+							</>
+					  )}
+				{checkUser && (
+					<div className="flex flex-col items-center justify-center gap-3 mt-4">
+						<Button
+							type="button"
+							kind="primary"
+							to={`/update/${productData.id}`}
+						>
+							Update product
+						</Button>
+					</div>
+				)}
+			</div>
+			<div className="flex flex-col justify-center my-4">
+				<div className="mb-6 text-left heading-text">More from this user</div>
+				<div className="grid grid-cols-4 gap-10 mx-auto">
+					{listProductData.slice(0, 8).map((item) => (
+						<Card
+							key={item.id}
+							to={`/buy/${item.id}`}
+							image={item?.image}
+							title={item?.Name}
+							address={productData?.createby?.address}
+							price={item?.Price}
+							avatar={productData?.createby?.avatar}
+						></Card>
+					))}
+				</div>
+				{listProductData.length > 8 ? (
+					<NavLink className={"text-right my-4"} to={`/artist/${creatorId}`}>
+						View all...{" "}
+					</NavLink>
+				) : (
+					<></>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default BuyPage;
--- my-app/src/pages/Collection/Collection.js
+++ my-app/src/pages/Collection/Collection.js
@@ -6,73 +6,73 @@ import { Button } from "components/button";
 import { Heading } from "components/heading";
 import { Pagination } from "components/pagination";
 const Collection = () => {
-  const [productList, setProductList] = useState([]);
-  const { userInfo } = useAuth();
-  const [option, setOption] = useState("create");
-  let createProduct = [];
-  let buyProduct = [];
-  useEffect(() => {
-    fetch("http://localhost:1337/products").then((response) =>
-      response.json().then((data) => {
-        setProductList(data);
-      })
-    );
-  }, []);
-  productList.forEach((item) => {
-    if (item.createby.id === userInfo.id) {
-      createProduct.push(item);
-    }
-    if (item.boughtby) {
-      if (item.boughtby.id === userInfo.id) {
-        buyProduct.push(item);
-      }
-    }
-  });
-  useEffect(() => {
-    document.title = "Collection";
-  }, []);
-  return (
-    <div className="container ">
-      <Heading title="Your Collection"></Heading>
-      <div className="flex flex-col justify-center my-4 mt-10 ">
-        <div className="flex option">
-          <Button
-            className="mb-6t"
-            onClick={() => setOption("create")}
-            active={option === "create"}
-            kind={"secondary"}
-            width="180px"
-          >
-            Created Product
-          </Button>
-          <Button
-            className="mb-6 ml-10"
-            onClick={() => setOption("buy")}
-            active={option === "buy"}
-            kind={"secondary"}
-            width="180px"
-          >
-            Buy Product
-          </Button>
-        </div>
-        {option === "create" ? (
-          createProduct.length !== 0 ? (
-            <Pagination items={createProduct}></Pagination>
-          ) : (
-            <div className="my-12 heading-text">
-              You have not created any products yet
-            </div>
-          )
-        ) : buyProduct.length !== 0 ? (
-          <Pagination items={buyProduct}></Pagination>
-        ) : (
-          <div className="my-12 heading-text">
-            You have not purchased any products yet
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	const [productList, setProductList] = useState([]);
+	const { userInfo } = useAuth();
+	const [option, setOption] = useState("create");
+	let createProduct = [];
+	let buyProduct = [];
+	useEffect(() => {
+		fetch("http://localhost:1337/products").then((response) =>
+			response.json().then((data) => {
+				setProductList(data);
+			}),
+		);
+	}, []);
+	productList.forEach((item) => {
+		if (item.createby.id === userInfo.id) {
+			createProduct.push(item);
+		}
+		if (item.boughtby) {
+			if (item.boughtby.id === userInfo.id) {
+				buyProduct.push(item);
+			}
+		}
+	});
+	useEffect(() => {
+		document.title = "Collection";
+	}, []);
+	return (
+		<div className="container ">
+			<Heading title="Your Collection"></Heading>
+			<div className="flex flex-col justify-center my-4 mt-10 ">
+				<div className="flex option">
+					<Button
+						className="mb-6t"
+						onClick={() => setOption("create")}
+						active={option === "create"}
+						kind={"secondary"}
+						width="180px"
+					>
+						Created
+					</Button>
+					<Button
+						className="mb-6 ml-10"
+						onClick={() => setOption("buy")}
+						active={option === "buy"}
+						kind={"secondary"}
+						width="180px"
+					>
+						Bought
+					</Button>
+				</div>
+				{option === "create" ? (
+					createProduct.length !== 0 ? (
+						<Pagination items={createProduct}></Pagination>
+					) : (
+						<div className="my-12 heading-text">
+							You have not created any products yet
+						</div>
+					)
+				) : buyProduct.length !== 0 ? (
+					<Pagination items={buyProduct}></Pagination>
+				) : (
+					<div className="my-12 heading-text">
+						You have not purchased any products yet
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Collection;
