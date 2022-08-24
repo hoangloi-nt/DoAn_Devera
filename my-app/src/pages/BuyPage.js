d01278cb13617abcf134d4857964327a32eab6fa
UIT19520097 <67829285+UIT19520097@users.noreply.github.com>
2022-08-24 22:03:43 +0700

    #12 update title BuyPage

--- my-app/src/pages/BuyPage.js
+++ my-app/src/pages/BuyPage.js
@@ -10,20 +10,18 @@ import { transfer } from "sdk/iconSDK.js";
 import { useAuth } from "components/contexts/auth-context";
 import { useTranslation } from "react-i18next";
 const BuyPage = () => {
-
-  const { t } = useTranslation();
-  const { nftId } = useParams();
-  const [productData, setProductData] = useState({});
-  const [listProductData, setListProductData] = useState([]);
-  const [sold, setSold] = useState(false);
-  const { userInfo } = useAuth();
-  const [checkUser, setCheckUser] = useState(false);
-  const sendToken = async (address, price) => {
-    const transferSuccess = await transfer({
-      to: address,
-      value: price,
-    });
-
+	const { t } = useTranslation();
+	const { nftId } = useParams();
+	const [productData, setProductData] = useState({});
+	const [listProductData, setListProductData] = useState([]);
+	const [sold, setSold] = useState(false);
+	const { userInfo } = useAuth();
+	const [checkUser, setCheckUser] = useState(false);
+	const sendToken = async (address, price) => {
+		const transferSuccess = await transfer({
+			to: address,
+			value: price,
+		});
 
 		if (transferSuccess === true) {
 			console.log("Done");
@@ -53,20 +51,18 @@ const BuyPage = () => {
 		document.body.scrollIntoView({ behavior: "smooth", block: "start" });
 	}, [nftId]);
 
-
-  async function updateProduct(id, data) {
-    try {
-      await axios.put(`http://localhost:1337/products/${id}`, {
-        boughtby: data,
-      });
-      toast.success(t("buyPage.succes"));
-    } catch (error) {
-      console.log(error);
-      toast.error(t("buyPage.fail"), error.message);
-    }
-  }
-  const creatorId = productData?.createby?.id;
-
+	async function updateProduct(id, data) {
+		try {
+			await axios.put(`http://localhost:1337/products/${id}`, {
+				boughtby: data,
+			});
+			toast.success(t("buyPage.succes"));
+		} catch (error) {
+			console.log(error);
+			toast.error(t("buyPage.fail"), error.message);
+		}
+	}
+	const creatorId = productData?.createby?.id;
 
 	useEffect(() => {
 		if (!creatorId) return;
@@ -90,80 +86,81 @@ const BuyPage = () => {
 		document.title = "Buy Page";
 	}, []);
 
-  return (
-    <div className="container ">
-      <div className="flex flex-col items-center justify-center mt-3">
-        <div className="mb-4 heading-text">{t("buyPage.title")}</div>
-        <Card
-          to={"#"}
-          image={productData?.image}
-          title={productData?.Name}
-          address={productData?.createby?.address}
-          price={productData?.Price}
-          avatar={productData?.createby?.avatar}
-        ></Card>
-        <div className="my-6 message-text">{t("buyPage.desc")}</div>
-        {sold
-          ? !checkUser && (
-              <Button kind="primary" width={"183px"} disabled>
-                {t("buyPage.soldOut")}
-              </Button>
-            )
-          : !checkUser && (
-              <Button
-                kind="primary"
-                width={"183px"}
-                onClick={() =>
-                  onSubmit(
-                    productData.createby.address,
-                    Number(productData.Price)
-                  )
-                }
-              >
-                {t("buyPage.buy")}
-              </Button>
-            )}
-        {checkUser && (
-          <div className="flex justify-center items-center gap-3 flex-col">
-            <span className="font-bold px-3 py-2 rounded-lg bg-red-400">
-              {t("buyPage.noBuy")}
-            </span>
-            <Button
-              type="button"
-              kind="primary"
-              to={`/update/${productData.id}`}
-            >
-              {t("buyPage.updateBtn")}
-            </Button>
-          </div>
-        )}
-      </div>
-      <div className="flex flex-col justify-center my-4">
-        <div className="mb-6 text-left heading-text">{t("buyPage.more")}</div>
-        <div className="grid grid-cols-4 gap-10 mx-auto">
-          {listProductData.slice(0, 8).map((item) => (
-            <Card
-              key={item.id}
-              to={`/buy/${item.id}`}
-              image={item?.image}
-              title={item?.Name}
-              address={productData?.createby?.address}
-              price={item?.Price}
-              avatar={productData?.createby?.avatar}
-            ></Card>
-          ))}
-        </div>
-        {listProductData.length > 8 ? (
-          <NavLink className={"text-right my-3"} to={"/"}>
-            {t("buyPage.viewAll")}
-          </NavLink>
-        ) : (
-          <></>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="container ">
+			<div className="flex flex-col items-center justify-center mt-3">
+				{sold ? (
+					<div className="mb-4 heading-text">{t("buyPage.soldTitle")}</div>
+				) : checkUser ? (
+					<div className="mb-4 heading-text">{t("buyPage.ownTitle")}</div>
+				) : (
+					<div className="mb-4 heading-text">{t("buyPage.buyTitle")}</div>
+				)}
 
+				<Card
+					to={"#"}
+					image={productData?.image}
+					title={productData?.Name}
+					address={productData?.createby?.address}
+					price={productData?.Price}
+					avatar={productData?.createby?.avatar}
+				></Card>
+				{sold
+					? !checkUser && <div className="my-6"></div>
+					: !checkUser && (
+							<>
+								<div className="my-6 message-text">{t("buyPage.desc")}</div>
+								<Button
+									kind="primary"
+									width={"183px"}
+									onClick={() =>
+										onSubmit(
+											productData.createby.address,
+											Number(productData.Price),
+										)
+									}
+								>
+									{t("buyPage.buy")}
+								</Button>
+							</>
+					  )}
+				{checkUser && (
+					<div className="flex flex-col items-center justify-center gap-3 my-6">
+						<Button
+							type="button"
+							kind="primary"
+							to={`/update/${productData.id}`}
+						>
+							{t("buyPage.updateBtn")}
+						</Button>
+					</div>
+				)}
+			</div>
+			<div className="flex flex-col justify-center my-4">
+				<div className="mb-6 text-left heading-text">{t("buyPage.more")}</div>
+				<div className="grid grid-cols-4 gap-10 mx-auto">
+					{listProductData.slice(0, 8).map((item) => (
+						<Card
+							key={item.id}
+							to={`/buy/${item.id}`}
+							image={item?.image}
+							title={item?.Name}
+							address={productData?.createby?.address}
+							price={item?.Price}
+							avatar={productData?.createby?.avatar}
+						></Card>
+					))}
+				</div>
+				{listProductData.length > 8 ? (
+					<NavLink className={"text-right my-3"} to={`/artist/${creatorId}`}>
+						{t("buyPage.viewAll")}
+					</NavLink>
+				) : (
+					<></>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default BuyPage;
